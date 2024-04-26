22822819a93cf401438e0f93ad942c4e2d81a4d2
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-26 20:52:41 +0000

    Add Load Payements component and related files

---
+++ frontend/src/app/load-payements/load-payements.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-load-payements',
+  templateUrl: './load-payements.component.html',
+  styleUrls: ['./load-payements.component.css']
+})
+export class LoadPayementsComponent {
+
+}
